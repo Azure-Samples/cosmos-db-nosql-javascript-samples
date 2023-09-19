10effe9e791efc279068ea13b7523ac814bd6711
Sidney Andrews <sidandrews@microsoft.com>
2023-09-19 12:03:14 -0400

    Add code block comments

--- 601-emulator/app.js
+++ 601-emulator/app.js
@@ -1,10 +1,15 @@
+// <imports>
 import { CosmosClient } from '@azure/cosmos'
+// </imports>
 
+// <client>
 const cosmosClient = new CosmosClient({
   endpoint: 'https://localhost:8081/',
   key: 'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=='
 })
+// </client>
 
+// <resources>
 const { database } = await cosmosClient.databases.createIfNotExists({
   id: 'cosmicworks',
   throughput: 400
@@ -18,10 +23,13 @@ const { container } = await database.containers.createIfNotExists({
     ]
   }
 })
+// </resources>
 
+// <upsert>
 const item = {
   id: '68719518371',
   name: 'Kiama classic surfboard'
 }
 
 container.items.upsert(item)
+// </upsert>
